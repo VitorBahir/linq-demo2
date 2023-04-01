3fc4f2a03a2d923a6ee02cfd5b4bae5fb7bc3c45
Vitor Bahir de Andrade <105059969+VitorBahir@users.noreply.github.com>
2023-04-01 14:54:42 -0300

    Initial implementation

---
+++ Course/Course/Program.cs
@@ -0,0 +1,112 @@
+﻿using Course.Entities;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Course
+{
+    class Program
+    {
+        static void Print<T>(string message, IEnumerable<T> collection)
+        {
+            Console.WriteLine(message);
+            foreach (T item in collection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2 };
+            Category c2 = new Category() { Id = 2, Name = "Computers", Tier = 1 };
+            Category c3 = new Category() { Id = 3, Name = "Electronics", Tier = 1 };
+
+            List<Product> products = new List<Product>() {
+                new Product() { Id = 1, Name = "Computer", Price = 1100.0, Category = c2 },
+                new Product() { Id = 2, Name = "Hammer", Price = 90.0, Category = c1 },
+                new Product() { Id = 3, Name = "TV", Price = 1700.0, Category = c3 },
+                new Product() { Id = 4, Name = "Notebook", Price = 1300.0, Category = c2 },
+                new Product() { Id = 5, Name = "Saw", Price = 80.0, Category = c1 },
+                new Product() { Id = 6, Name = "Tablet", Price = 700.0, Category = c2 },
+                new Product() { Id = 7, Name = "Camera", Price = 700.0, Category = c3 },
+                new Product() { Id = 8, Name = "Printer", Price = 350.0, Category = c3 },
+                new Product() { Id = 9, Name = "MacBook", Price = 1800.0, Category = c2 },
+                new Product() { Id = 10, Name = "Sound Bar", Price = 700.0, Category = c3 },
+                new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1 }
+            };
+
+            //var r1 = products.Where(x => x.Price < 900.0 && x.Category.Tier == 1);
+            var r1 =
+                from p in products
+                where p.Category.Tier == 1 && p.Price < 900.0
+                select p;
+            Print("Tier 1 AND PRICE < 900.0:", r1);
+            Console.WriteLine();
+
+            //var r2 = products.Where(x => x.Category.Name == "Tools").Select(x => x.Name);
+            var r2 =
+                from p in products
+                where p.Category.Name == "Tools"
+                select p.Name;
+            Print("NAMES OF PRODUCTS FROM TOOLS:", r2);
+            Console.WriteLine();
+
+            //var r3 = products.Where(x => x.Name[0] == 'C').Select(x => new { x.Name, x.Price, CategoryName = x.Category.Name });
+            var r3 =
+                from p in products
+                where p.Name[0] == 'C'
+                select new { 
+                    p.Name,
+                    p.Price,
+                    CategoryName = p.Category.Name };
+            Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT:", r3);
+            Console.WriteLine();
+
+            //var r4 = products.Where(x => x.Category.Tier == 1).OrderBy(x => x.Price).ThenBy(x => x.Name);
+            var r4 =
+                from p in products
+                where p.Category.Tier == 1
+                orderby p.Name
+                orderby p.Price
+                select p;
+            Print("TIER 1 ORDER BY PRICE THEN BY NAME", r4);
+            Console.WriteLine();
+
+            //var r5 = r4.Skip(2).Take(4);
+            var r5 = 
+                (from p in r4
+                select p).Skip(2).Take(4);
+            Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", r5);
+            Console.WriteLine();
+
+            //var r6 = products.First();
+            var r6 =
+                (from p in products
+                 select p).First();
+            Console.WriteLine("First test1: " + r6);
+            //var r7 = products.Where(x => x.Price > 3000.0).FirstOrDefault();
+            var r7 =
+            (from p in products
+             where p.Price > 3000.0
+             select p).FirstOrDefault();
+            Console.WriteLine("First or default test2: " + r7);
+            Console.WriteLine();
+
+            //var r8 = products.GroupBy(x => x.Category);
+            var r8 =
+                (from p in products
+                 group p by p.Category);
+            foreach (IGrouping<Category, Product> group in r8)
+            {
+                Console.WriteLine("Category: " + group.Key.Name);
+                foreach (Product p in group)
+                {
+                    Console.WriteLine(p);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
